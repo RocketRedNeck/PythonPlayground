d1917657e6a5796d3fef0dc6454ee5a0cb19db81
RocketRedNeck <mtkessel@mac.com>
2016-09-28 07:16:03 -0700

    Split pidSim experiments
    pidSim2 is now 1 integration (velocity control variable) and pidSim3 is 2 integrations (acceleration or torque control variable)

--- pidSim3.py
+++ pidSim3.py
@@ -7,7 +7,8 @@ processing latency and variation; demonstrates the impact of variation
 to successful control.
 
 THIS VERSION models the control as a second order input (torque to acceleration)
-and then integrates once to get position. In other words, the control
+and then integrates twice to get position. The result is that derivative control
+will be required account for the integration lag. In other words, the control
 variable (CV) has indirect control over the process variable (PV)
 
 This allows students to experiment with how different elements in the scaling
@@ -61,9 +62,9 @@ nmax = ts_sec.__len__() # round(tmax_sec/dt_sec)
 ns = range(0, nmax)
 
 
-kp = 0.5    # Proportional gain
-ki = 0    # Integral gain
-kd = 0.1   # Derivative gain
+kp = 0.0    # Proportional gain
+ki = 0.0    # Integral gain
+kd = 0.4   # Derivative gain
 kg = 1.0    # Plant (Process) gain
 
 tau_sec   = 0.1
@@ -81,9 +82,13 @@ G = np.zeros(nmax)     # Process output to be measured
 
 exp = np.exp(-dt_sec/tau_sec)
 
+# Define a mass property for scaling later
+# in this case we just use a scalar to represent either linear or rotation
+m = 1
 
 # Define arrays to hold the kinematic values
 # In this case we will use simple names to represent either linear or rotation
+a = np.zeros(nmax)     # linear or angular acceleration
 v = np.zeros(nmax)     # linear or angular velocity
 p = np.zeros(nmax)     # linear or angular position
 
@@ -281,13 +286,16 @@ for n in ns:
     #
     # The form is F = m a or M = I alpha, but we will just use simple names
     # here
-    v[n] = G[n]
+    a[n] = G[n] / m
     
-    # Integrate to a position
+    # Integrate to a velocity
     # Here will use a simple trapezoidal rule; we can upgrade this to Runge-Kutta
     # or other methods later but if our time step is small enough compared to
     # the rate of change, then trapezoidal is fine
+    if (n > 0):
+        v[n] = v[n-1] + a[n-1]*dt_sec + (a[n] - a[n-1])*dt_sec/2
 
+    # Integrate to a position
     if (n > 0):
         p[n] = p[n-1] + v[n-1]*dt_sec + (v[n] + v[n-1])*dt_sec/2
     
