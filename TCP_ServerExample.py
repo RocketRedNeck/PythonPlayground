d51ce2a4b68b0eef796890ce351fc2f212d6e385
mtkessel <mtkessel@mac.com>
2017-10-28 08:41:10 -0700

    Minor cleaup

--- TCP_ServerExample.py
+++ TCP_ServerExample.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 23 12:54:47 2017
+TCP Server Example
 
-@author: mtkes
+Copyright (c) 2017 - Michael Kessel (mailto: the.rocketredneck@gmail.com)
+a.k.a. RocketRedNeck, RocketRedNeck.com, RocketRedNeck.net 
+
+RocketRedNeck and MIT Licenses 
+
+RocketRedNeck hereby grants license for others to copy and modify this source code for 
+whatever purpose other's deem worthy as long as RocketRedNeck is given credit where 
+where credit is due and you leave RocketRedNeck out of it for all other nefarious purposes. 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions: 
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software. 
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE. 
+****************************************************************************************************
 """
 
 import socket
@@ -30,6 +56,8 @@ import socket
 #   '224.0.0.0' to '239.255.255.255' is reserved for multicast
 #
 IP_ANY = '127.0.0.1'     # In this case we don't care what address our hardware has
+IP_LOOPBACK = '127.0.0.1'
+IP_LOCAL = IP_ANY
 
 # Ports are a way of distinguishing the type of information traffic on a
 # netowrk. The port is simply a logical distinction that allows traffic to
@@ -75,7 +103,7 @@ while (True):
     # Ports are a way of identifying packets to be routed to specific
     # user path on an interface
     print("Binding...")
-    listeningSocket.bind((IP_ANY, PORT))
+    listeningSocket.bind((IP_LOCAL, PORT))
     
     # Now we set up listening to wait for a connection request
     # We set the backlog to 0 to allow up to 0 unaccepted connection
@@ -87,7 +115,7 @@ while (True):
     print("Listen...")
     listeningSocket.listen(0)
     
-    listeningSocket.settimeout(5.0)
+    listeningSocket.settimeout(1.0)
 
     try:   
         acceptedSocket, addressInfo = listeningSocket.accept()
@@ -109,7 +137,7 @@ while (True):
             while (True):
                 try:
                     i = i + 1
-                    s = 'String = ' + str(i + 1)
+                    s = 'String = ' + str(i + 1) + '*'
                     print(s)
                     acceptedSocket.send(str.encode(s))
                 except Exception as e:
